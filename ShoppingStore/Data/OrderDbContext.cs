a7229aa4ae05996b91a9877358fd89c4c5dd03e1
mabbasidesign <mabbasidesign.gmil.com>
2021-07-20 12:56:29 +0430

    Add DbContext

---
+++ ShoppingStore/Data/OrderDbContext.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ShoppingStore.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ShoppingStore.Data
+{
+    public class OrderDbContext: DbContext
+    {
+        public OrderDbContext(DbContextOptions<OrderDbContext> options)
+        {
+
+        }
+
+        public virtual DbSet<Customer> Customers { get; set; }
+        public virtual DbSet<Employee> Employees { get; set; }
+        public virtual DbSet<Notification> Notifications { get; set; }
+        public virtual DbSet<Order> Orders { get; set; }
+        public virtual DbSet<OrderProduct> OrderProducts { get; set; }
+        public virtual DbSet<Position> Positions { get; set; }
+        public virtual DbSet<Product> Products { get; set; }
+        public virtual DbSet<Saller> Sallers { get; set; }
+
+    }
+}
