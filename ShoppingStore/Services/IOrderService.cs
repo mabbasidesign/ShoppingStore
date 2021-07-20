0bae295778d2c0db14cd914fde61f6e295d678f8
mabbasidesign <mabbasidesign.gmil.com>
2021-07-20 15:47:17 +0430

    Implement Order Service

--- ShoppingStore/Services/IOrderService.cs
+++ ShoppingStore/Services/IOrderService.cs
@@ -8,10 +8,11 @@ namespace ShoppingStore.Services
 {
     public interface IOrderService
     {
-        void CreateOrderByCustomer(Order oder);
-        void UpdateOrderBySaller(Order oder); // Update And Edit Order By Seller
-        void UpdateAndConfirmOrderBySalesManager(Order oder); // Update Order by sales manager and confirm or denay
-        IEnumerable<Order> GetConfirmOrders(); // Display All Orders if they are confirm by sales manager
+        IEnumerable<Order> GetOrders();
+        void CreateOrderByCustomer(Order order); // Create order by customer
+        void UpdateOrderBySaller(Order order); // Update And Edit Order By Seller
+        bool UpdateAndConfirmOrderBySalesManager(Order order); // Update Order by sales manager and confirm or denay
+        bool GetConfirmOrders(); // Display All Orders if they are confirm by sales manager
         IEnumerable<Order> DisplayNewOrdersToManager(); // display new orders (less than 24-h) which are confirmed to the eo
     }
 }
---
+++ ShoppingStore/Services/OrderService.cs
@@ -0,0 +1,93 @@
+﻿using ShoppingStore.Data;
+using ShoppingStore.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ShoppingStore.Services
+{
+    public class OrderService: IOrderService
+    {
+        private OrderDbContext _db;
+        public OrderService(OrderDbContext db)
+        {
+            _db = db;
+        }
+
+        public IEnumerable<Order> GetOrders()
+        {
+            return _db.Orders.OrderBy(o => o.DateTime).ToList();
+        }
+
+        // Create order by Customer
+        public void CreateOrderByCustomer(Order order)
+        {
+            _db.Orders.Add(order);
+            _db.SaveChanges();
+        }
+
+        // Update order by seller
+        public void UpdateOrderBySaller(Order order)
+        {
+            try
+            {
+                if (_db.Employees.Any(e => e.Position.Name == "Employee"))
+                {
+                    _db.Update(order);
+                    _db.SaveChanges();
+                }
+            }
+            catch (InvalidCastException e)
+            {
+                throw new InvalidCastException("Only Employees can Edit this order", e);
+            }
+        }
+
+        // Confirm order by sales manager
+        public bool UpdateAndConfirmOrderBySalesManager(Order order)
+        {
+            try
+            {
+                if (_db.Employees.Any(e => e.Position.Name == "Sales Manager"))
+                {
+                    _db.Update(order);
+                    _db.SaveChanges();
+                }
+            }
+            catch (InvalidCastException e)
+            {
+                throw new InvalidCastException("Only Sales Manager can Edit this order", e);
+            }
+
+            return _db.Orders.Any(o => o.IsConfirmBySalesManger == true) ? true : false;
+
+        }
+
+        // Display All Orders if they are confirm by sales manager
+        public bool GetConfirmOrders()
+        {
+            var isConfirmed = _db.Orders.Any(o => o.IsConfirmBySalesManger == true);
+
+            if (isConfirmed)
+            {
+                GetOrders();
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        // display new orders (less than 24-h) that are confirmed
+        public IEnumerable<Order> DisplayNewOrdersToManager()
+        {
+            var now = DateTime.Now;
+
+            return _db.Orders.Where(o => (now - o.DateTime).TotalHours < 24)
+                .OrderByDescending(o => o.DateTime);
+        }
+
+    }
+}
