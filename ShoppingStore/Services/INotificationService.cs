5245242bb471d75e576b6bdcee689b732a97b5c1
mabbasidesign <mabbasidesign.gmil.com>
2021-07-20 12:28:13 +0430

    Add order & notification interface

---
+++ ShoppingStore/Services/INotificationService.cs
@@ -0,0 +1,15 @@
+﻿using ShoppingStore.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ShoppingStore.Services
+{
+    public interface INotificationService
+    {
+        void EmployeeNotifications(Notification notification); // Send confirm order sms to corespondent employee if position == employee using thread
+        IEnumerable<Order> NotificationDateAndStatus(); // display all notifications
+        void CEONotifications(Notification notification); // Send sms to ceo manages every night at 8pm
+    }
+}
---
+++ ShoppingStore/Services/IOrderService.cs
@@ -0,0 +1,17 @@
+﻿using ShoppingStore.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ShoppingStore.Services
+{
+    public interface IOrderService
+    {
+        void CreateOrderByCustomer(Order oder);
+        void UpdateOrderBySaller(Order oder); // Update And Edit Order By Seller
+        void UpdateAndConfirmOrderBySalesManager(Order oder); // Update Order by sales manager and confirm or denay
+        IEnumerable<Order> GetConfirmOrders(); // Display All Orders if they are confirm by sales manager
+        IEnumerable<Order> DisplayNewOrdersToManager(); // display new orders (less than 24-h) which are confirmed to the eo
+    }
+}
