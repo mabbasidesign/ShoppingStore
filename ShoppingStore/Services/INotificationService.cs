5c6c4fc4916e4b398174d753c84d5e6850611141
mabbasidesign <mabbasidesign.gmil.com>
2021-07-22 11:49:15 +0430

    uncomment

--- ShoppingStore/Services/INotificationService.cs
+++ ShoppingStore/Services/INotificationService.cs
@@ -8,8 +8,8 @@ namespace ShoppingStore.Services
 {
     public interface INotificationService
     {
-        bool EmployeeNotifications(Notification notification, int EmployeeId); // Send confirm order sms to corespondent employee if position == employee using thread
-        IEnumerable<Notification> NotificationDateAndStatus(); // display all notifications
-        bool CEONotifications(Notification notification, int EmployeeId); // Send sms to ceo manages every night at 8pm
+        bool EmployeeNotifications(Notification notification, int EmployeeId);
+        IEnumerable<Notification> NotificationDateAndStatus();
+        bool CEONotifications(Notification notification, int EmployeeId);
     }
 }
--- ShoppingStore/Services/IOrderService.cs
+++ ShoppingStore/Services/IOrderService.cs
@@ -9,10 +9,10 @@ namespace ShoppingStore.Services
     public interface IOrderService
     {
         IEnumerable<Order> GetOrders();
-        void CreateOrderByCustomer(Order order); // Create order by customer
-        void UpdateOrderBySaller(Order order); // Update And Edit Order By Seller
-        bool UpdateAndConfirmOrderBySalesManager(Order order); // Update Order by sales manager and confirm or denay
-        bool GetConfirmOrders(); // Display All Orders if they are confirm by sales manager
-        IEnumerable<Order> DisplayNewOrdersToManager(); // display new orders (less than 24-h) which are confirmed to the eo
+        void CreateOrderByCustomer(Order order); 
+        void UpdateOrderBySaller(Order order);
+        bool UpdateAndConfirmOrderBySalesManager(Order order);
+        bool GetConfirmOrders();
+        IEnumerable<Order> DisplayNewOrdersToManager();
     }
 }
